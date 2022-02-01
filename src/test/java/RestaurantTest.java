d99257db77b05aa13c35bcd9c88d9c6e7f55c364
bs <bs@hpe.com>
2022-02-02 01:37:49 +0530

    Added test cases

---
+++ src/test/java/RestaurantTest.java
@@ -0,0 +1,84 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantTest {
+    Restaurant restaurant;
+    List<Item> menuItem = new ArrayList<Item>();
+    //REFACTOR ALL THE REPEATED LINES OF CODE
+    public void restaurantCreation(){
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant = new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+    }
+    //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
+    @Test
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+        restaurantCreation();
+        assertTrue(restaurant.isRestaurantOpen());
+    }
+
+    @Test
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+        restaurantCreation();
+        assertFalse(restaurant.isRestaurantOpen());
+
+    }
+
+    //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    @Test
+    public void total_price_of_the_items_selected(){
+        restaurantCreation();
+        menuItem = restaurant.getMenu();
+        assertEquals(388,restaurant.getSelectedItemValue(menuItem));
+    }
+    @Test
+    public void failing_test_case_total_price_of_the_items_selected(){
+        restaurantCreation();
+        menuItem = restaurant.getMenu();
+        assertEquals(380,restaurant.getSelectedItemValue(menuItem));
+    }
+
+    @Test
+    public void total_price_of_the_items_when_an_item_is_removed(){
+        restaurantCreation();
+        menuItem = restaurant.getMenu();
+        int price = restaurant.getSelectedItemValue(menuItem);
+        int newPrice = menuItem.get(1).getPrice();
+        menuItem.remove(1);
+        assertEquals(price-newPrice,restaurant.getSelectedItemValue(menuItem));
+    }
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void adding_item_to_menu_should_increase_menu_size_by_1(){
+        restaurantCreation();
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.addToMenu("Sizzling brownie",319);
+        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+        restaurantCreation();
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.removeFromMenu("Vegetable lasagne");
+        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_that_does_not_exist_should_throw_exception() {
+        restaurantCreation();
+
+        assertThrows(itemNotFoundException.class,
+                ()->restaurant.removeFromMenu("French fries"));
+    }
+    //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+}
